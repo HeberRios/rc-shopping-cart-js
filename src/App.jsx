725280d074f30985d340359ea36951d9bc1a6300
HeberRios <hebera.riosg@gmail.com>
2025-01-13 12:29:08 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+      <img
+        src='../src/assets/icons/shopping-cart.svg'
+        alt='white shopping cart'
+        className='cart-icon'
+      />
+    </>
+  );
+}
+
+export default App;
