0ee162c7fd51461eb2cc176516c23ea990fd2572
HeberRios <hebera.riosg@gmail.com>
2025-01-15 16:09:10 -0600

    Add styles to the filters component
    Also some styles were added to the product item, for a better
    presentation.

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,14 +1,21 @@
 export function Filters() {
   return (
     <section className='filters-container container'>
-      <input type='range' name='price-range' id='price-range' />
-      <select name='category' id='category'>
-        <option value='all'>All</option>
-        <option value='beauty'>Beauty</option>
-        <option value='fragrances'>Fragrances</option>
-        <option value='furniture'>Furniture</option>
-        <option value='groceries'>Groceries</option>
-      </select>
+      <label htmlFor='price-range'>
+        Starting price:
+        <input type='range' name='price-range' id='price-range' />
+      </label>
+
+      <label htmlFor='category:'>
+        Category:
+        <select name='category' id='category'>
+          <option value='all'>All</option>
+          <option value='beauty'>Beauty</option>
+          <option value='fragrances'>Fragrances</option>
+          <option value='furniture'>Furniture</option>
+          <option value='groceries'>Groceries</option>
+        </select>
+      </label>
     </section>
   );
 }
