21f6b0769892b7915d0d3b020b7b671bd371662c
HeberRios <hebera.riosg@gmail.com>
2025-01-20 15:28:38 -0600

    Consume the context in the Products component
    The cart functionality was implemented in the Products component, now
    when you press the add to cart button, will be added to the cart but
    also the color and icon inside the button will change.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,37 @@
-import { AddToCartIcon } from './Icons';
+import { useCart } from '../hooks/useCart';
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons';
 
 export function Products({ products }) {
+  const { cart, addToCart, removeFromCart } = useCart();
+
+  function checkProductIsInCart(product) {
+    return cart.some(function (item) {
+      return item.id === product.id;
+    });
+  }
+
   return (
     <main className='products-container container'>
       <ul className='products-list'>
         {products.map(function (product) {
+          const isProductInCart = checkProductIsInCart(product);
+
           return (
             <li key={product.id} className='product-item'>
               <img src={product.thumbnail} alt={product.title} />
               <h2>{product.title}</h2>
               <p>{product.description.slice(0, 80).trim().concat('...')}</p>
               <span>${product.price}</span>
-              <button className='btn'>{<AddToCartIcon />}</button>
+              <button
+                className={isProductInCart ? 'btn remove-bg' : 'btn'}
+                onClick={function () {
+                  isProductInCart
+                    ? removeFromCart(product)
+                    : addToCart(product);
+                }}
+              >
+                {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
+              </button>
             </li>
           );
         })}
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,11 +1,6 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/cart';
 
-// we are going to use the useCart custom hook to provide the context where is
-// needed
-
-// as a good practice is good to check if the context is being requested inside
-// the context provider
 export function useCart() {
   const context = useContext(CartContext);
 
