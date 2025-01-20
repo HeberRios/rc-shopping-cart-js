86abf45bad6d799b1681d750037ca5228b4be457
HeberRios <hebera.riosg@gmail.com>
2025-01-20 12:31:47 -0600

    Create the reducer function

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,38 +1,22 @@
-// we need to create the reducer function that will be used in the useReducer hook
-// but first we need the action types and the actions to be performed:
+// We also create an initial state for the cart
 
-// the action types that will be performed by the userReducer (with dispatch function)
+const cartInitialState = [];
+
 const CART_ACTION_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   CLEAR_CART: 'CLEAR_CART',
 };
 
-// the actual actions to be performed depending on the type of action provided
 const STATE_UPDATE_BY_ACTION = {
-  // this methods receive the state and an action, because to implement
-  // the useReducer hook, we need to use a reducer function and this function
-  // receives two arguments the current state and an action object with the info
-  // of the action to perform
-
   [CART_ACTION_TYPES.ADD_TO_CART]: function (state, actionObj) {
-    // here we are extracting the id of the product we want to add
-    // and the payload property of the actionObj is the product itself with
-    // all its properties but we are only extracting its id using the destructuring
-    // assignment
     const { id } = actionObj.payload;
 
     const productInCartIndex = state.findIndex(function (item) {
       return item.id === id;
     });
 
-    // if the product is in the cart the index will be greater or equal than 0
     if (productInCartIndex >= 0) {
-      //  so we get all the products in the cart BEFORE the product being added,
-      // then as the product is already in the cart, this will be also extracted from
-      // the state but we will increment its quantity property by 1 and finally we
-      // extract all the products that were after the product in question, in this case
-      // this product is the one that will get its quantity increased
       const newState = [
         ...state.slice(0, productInCartIndex),
 
@@ -44,12 +28,9 @@ const STATE_UPDATE_BY_ACTION = {
         ...state.slice(productInCartIndex + 1),
       ];
 
-      // all this is the new state for the cart
       return newState;
     }
 
-    // if the product that triggered this action is not in the cart, then extract the cart state
-    // and then add the new product and create a new property called quantity for it
     const newState = [...state, { ...actionObj.payload, quantity: 1 }];
 
     return newState;
@@ -72,3 +53,15 @@ const STATE_UPDATE_BY_ACTION = {
     return [];
   },
 };
+
+export function cartReducer(state, actionObj) {
+  //  we get the action type from the action obj
+  const { type: actionType } = actionObj;
+
+  // we get the action to be performed depending on the action type
+  const updateState = STATE_UPDATE_BY_ACTION[actionType];
+
+  // if the action exists in the STATE_UPDATE_BY_ACTION object we pass
+  // the state and the action object for the use of the action method
+  return updateState ? updateState(state, actionObj) : state;
+}
